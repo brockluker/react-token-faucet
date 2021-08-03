9982922ddaa083b0b32230e59d4025b50fcbc560
brock cameron <brockluker@gmail.com>
2021-08-02 21:51:27 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Faucet from './components/Faucet.js'
+import FCTToken from './artifacts/contracts/FCTToken.sol/FCTToken.json'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container, Row, Col } from 'react-bootstrap'
+
+function App() {
+
+  const Token = FCTToken;
+
+  return (
+    <div className="App">
+    <Container>
+    <Row className="justify-content-md-center">
+      <Col>
+      <div>our faucet</div>
+      </Col>
+      <Col>
+      <div> our send area</div>
+      </Col>
+    </Row>
+    </Container>
+    <Faucet  tokenContract={Token}/>
+    </div>
+  );
+}
+
+export default App;
